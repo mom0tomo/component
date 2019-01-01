133fd70def6de6597aa211247cc25950e82dd2d8
mom0tomo <momo76w@gmail.com>
2019-01-01 11:33:24 +0900

    Excersize: Components

--- src/index.js
+++ src/index.js
@@ -1,32 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApproveCard';
+import Message from './Message';
 
 const App = () => {
 	return (
-		<div className="ui container comments">
-			<ApprovalCard>
-				<div>
-					<h4>Warning</h4>
-					Are you sure you want to do this?
-				</div>
-			</ApprovalCard>
-			<ApprovalCard>
-				<CommentDetail
-					avatar={faker.image.avatar()}
-					author="Sam"
-					timeAgo="Today 6:00PM"
-					content="this is nice!"
-				/>
-			</ApprovalCard>
-			<ApprovalCard>
-				<CommentDetail avatar={faker.image.avatar()} author="Alex" timeAgo="Today 6:01PM" content="awesome" />
-			</ApprovalCard>
-			<ApprovalCard>
-				<CommentDetail avatar={faker.image.avatar()} author="Jane" timeAgo="Today 6:03PM" content="cool!" />
-			</ApprovalCard>
+		<div>
+			<Message
+				header="Changes in Service"
+				text="We just updated our privacy policy here to better service our customers."
+			/>
 		</div>
 	);
 };
